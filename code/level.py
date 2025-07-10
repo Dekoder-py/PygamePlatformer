9d9d80c93133a696856736c64fed64b4e6593471
Dekoder-py <dekod3r.py@gmail.com>
2025-07-11 10:24:41 +1200

    add basic main file and level file

---
+++ code/level.py
@@ -0,0 +1,9 @@
+from settings import *
+
+
+class Level:
+    def __init__(self):
+        self.display_surf = pygame.display.get_surface()
+
+    def run(self):
+        self.display_surf.fill("red")
