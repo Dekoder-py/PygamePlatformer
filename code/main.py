411396fce52c0c95acaf9fb1d004d76bccbcdf78
Dekoder-py <dekod3r.py@gmail.com>
2025-07-11 10:38:58 +1200

    load tmx maps

--- code/main.py
+++ code/main.py
@@ -1,3 +1,7 @@
+from os.path import join
+
+from pytmx.util_pygame import load_pygame
+
 from level import Level
 from settings import *
 
@@ -8,7 +12,9 @@ class Game:
         self.display_surf = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
         pygame.display.set_caption("Unnamed Platform World")
 
-        self.current_stage = Level()
+        self.tmx_maps = {0: load_pygame(join("data", "levels", "omni.tmx"))}
+
+        self.current_stage = Level(self.tmx_maps[0])
 
     def run(self):
         while True:
