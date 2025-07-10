6aba34bfc54f6935d32f8c70d6cf986a37d89151
Dekoder-py <dekod3r.py@gmail.com>
2025-07-11 09:30:32 +1200

    change entity class to use vectors and frects

--- scripts/entities.py
+++ scripts/entities.py
@@ -1,14 +1,19 @@
+from os.path import join
+
 import pygame
 
 
 class PhysicsEntity(pygame.sprite.Sprite):
-    def __init__(self, game, e_type, pos, size, *groups):
+    def __init__(self, game, e_type, pos, *groups):
         super().__init__(*groups)
         self.game = game
         self.type = e_type
-        self.pos = pygame.math.Vector2(pos)
-        self.size = size
-        self.velocity = [0, 0]
+        self.velocity = pygame.math.Vector2(1, 0)
+        self.image = pygame.image.load(
+            join("data", "images", "entities", "player.png")
+        ).convert()
+        self.rect = self.image.get_frect(center=pos)
+        self.image.set_colorkey("#000000")
 
     def update(self, movement=(0, 0)):
         frame_movement = pygame.math.Vector2(
@@ -16,5 +21,4 @@ class PhysicsEntity(pygame.sprite.Sprite):
             movement[1] + self.velocity[1],
         )
 
-        self.pos += frame_movement
-        print(frame_movement)
+        self.rect.center += frame_movement
