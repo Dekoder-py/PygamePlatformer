fb02bed753aef5e886c0e5e7ca0e7921fb1c4c38
Dekoder-py <dekod3r.py@gmail.com>
2025-07-10 21:16:17 +1200

    add game loop and initial setup for platformer

--- game.py
+++ game.py
@@ -1,5 +1,24 @@
+import sys
+
 import pygame
 
+# initial setup
 pygame.init()
 
-screen = pygame.display.set_mode(())
+pygame.display.set_caption("Unnamed Platformer")
+screen = pygame.display.set_mode((640, 480))
+
+clock = pygame.time.Clock()
+
+while True:
+    # event loop
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    screen.fill("#0fbfac")
+
+    # update the display and cap frame rate
+    pygame.display.update()
+    clock.tick(60)
