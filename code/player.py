685e55f10b10a172d29b6703e1c1e9ff27b06043
Dekoder-py <dekod3r.py@gmail.com>
2025-07-11 15:45:11 +1200

    add vertical collision checks

--- code/player.py
+++ code/player.py
@@ -52,7 +52,19 @@ class Player(pygame.sprite.Sprite):
                         self.rect.right = sprite.rect.left
                 # vertical
                 else:
-                    pass
+                    # up
+                    if (
+                        self.rect.top <= sprite.rect.bottom
+                        and self.old_rect.top >= sprite.old_rect.bottom
+                    ):
+                        self.rect.top = sprite.rect.bottom
+                    # down
+                    if (
+                        self.rect.bottom >= sprite.rect.top
+                        and self.old_rect.bottom <= sprite.old_rect.top
+                    ):
+                        self.rect.bottom = sprite.rect.top
+                    self.direction.y = 0
 
     def update(self, dt):
         self.old_rect = self.rect.copy()
