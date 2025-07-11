335c81c1e99d6396a3b1bbd8b67b9a415330c2a4
Dekoder-py <dekod3r.py@gmail.com>
2025-07-11 13:00:06 +1200

    add left/right basic collision

--- code/player.py
+++ code/player.py
@@ -2,7 +2,7 @@ from settings import *
 
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, pos, *groups):
+    def __init__(self, pos, groups, collision_sprites):
         super().__init__(*groups)
         self.image = pygame.Surface((48, 56))
         self.image.fill("red")
@@ -10,18 +10,39 @@ class Player(pygame.sprite.Sprite):
 
         # movement
         self.direction = vector()
-        self.speed = 200
+        self.speed = 300
+
+        # collision
+        self.collision_sprites = collision_sprites
 
     def input(self):
         keys = pygame.key.get_pressed()
-
-        self.direction.x = keys[pygame.K_RIGHT] - keys[pygame.K_LEFT]
-        self.direction = (
-            self.direction.normalize() if self.direction else self.direction
-        )
+        input_vector = vector()
+        if keys[pygame.K_RIGHT]:
+            input_vector.x += 1
+        if keys[pygame.K_LEFT]:
+            input_vector.x -= 1
+        self.direction = input_vector.normalize() if input_vector else input_vector
 
     def move(self, dt):
-        self.rect.topleft += self.direction * self.speed * dt
+        self.rect.x += self.direction.x * self.speed * dt
+        self.collision("horizontal")
+        self.rect.y += self.direction.y * self.speed * dt
+        self.collision("vertical")
+
+    def collision(self, axis):
+        for sprite in self.collision_sprites:
+            if sprite.rect.colliderect(self.rect):
+                if axis == "horizontal":
+                    # left
+                    if self.rect.left <= sprite.rect.right:
+                        self.rect.left = sprite.rect.right
+                    # right
+                    if self.rect.right >= sprite.rect.left:
+                        self.rect.right = sprite.rect.left
+                # vertical
+                else:
+                    pass
 
     def update(self, dt):
         self.input()
